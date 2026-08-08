153fc21968d95e9e5c69000d0a45a759d7815e17
Alan Babu <alanbabu518@gmail.com>
2026-08-08 18:29:58 +0530

    solved cors error

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin : "https://career-pilot-ai-orpin.vercel.app/",
+    origin : "https://career-pilot-ai-orpin.vercel.app",
     credentials : true
 }))
 
